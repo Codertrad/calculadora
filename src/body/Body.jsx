3df0c59f3d9f8f552a9f2f4a8f4813527ac9b001
codertrad <julianh98@outlook.com>
2023-01-06 15:07:19 -0500

    ended

--- src/body/Body.jsx
+++ src/body/Body.jsx
@@ -4,10 +4,10 @@ import { ButtonsContainer } from "../buttons/ButtonsContainer";
 
 export const Body = () => {
   return (
-      <Container>
-        <Screen />
-        <ButtonsContainer />
-      </Container>
+    <Container>
+      <Screen />
+      <ButtonsContainer />
+    </Container>
   );
 };
 
@@ -15,4 +15,13 @@ const Container = styled.div`
   background-color: #434242;
   border-radius: 10px;
   width: 80%;
+  @media (min-width: 768px) {
+    max-width: 50%;
+  }
+  @media (min-width: 1024px) {
+    max-width: 30%;
+  }
+    @media (min-width: 1440px) {
+    max-width: 20%;
+  }
 `;
